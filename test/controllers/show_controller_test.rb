3e57313a755ade1d79c69887921e7a7ef11a9ba7
ryouta <89526043+Zastenchivyy-Smolensky@users.noreply.github.com>
2022-01-09 23:35:49 +0900

    finish healper

--- test/controllers/show_controller_test.rb
+++ test/controllers/show_controller_test.rb
@@ -4,13 +4,13 @@ class ShowControllerTest < ActionDispatch::IntegrationTest
   test "should get top" do
     get show_top_url
     assert_response :success
-    assert_select "title","Topページ"
+    assert_select "title","ページ"
   end
   
 
   test "should get about" do
     get show_about_url
     assert_response :success
-    assert_select "title","詳細ページ"
+    assert_select "title","ページ"
   end
 end
